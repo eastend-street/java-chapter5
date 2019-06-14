6913020b9436702e58855fc3109f64b0ab1b6c71
eastend-street <ss_1322irf@icloud.com>
2019-06-14 13:49:26 -0700

    RepeatMultiplicationQuiz

---
+++ src/RepeatMultiplicationQuiz.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class RepeatMultiplicationQuiz {
+	public static void main(String[] args) {
+		int number1 = (int) (Math.random() * 10);
+		int number2 = (int) (Math.random() * 10);
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("What is " + number1 + " * " + number2 + " ?");
+		int answer = input.nextInt();
+
+		while (number1 * number2 != answer) {
+			System.out.println("Wrong answer, try again. What is " + number1 + " * " + number2 + " ?");
+			answer = input.nextInt();
+		}
+		
+		System.out.println("You got it!");
+	}
+}
